0113688a0a340396c1f5e23be369658ae8e371cc
JohnPrice4444 <94655821+JohnPrice4444@users.noreply.github.com>
2022-05-02 13:50:03 -0500

    added stuff to alternate map

--- frontend/doggy-pile/src/pages/map/AlternateMap.js
+++ frontend/doggy-pile/src/pages/map/AlternateMap.js
@@ -1,4 +1,4 @@
-import Map from 'react-map-gl';
+import Map, { Layer, Source, NavigationControl, GeolocateControl } from 'react-map-gl';
 import { useEffect, useState, useRef, useCallback } from 'react'
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 import axios from 'axios';
@@ -10,18 +10,30 @@ function AlternateMap(props) {
 
   useEffect(() => {
     dogParkApiCall()
-  })
+  }, [])
 
   const dogParkApiCall = () => {
-    axios.get(`https://api.geoapify.com/v2/places?categories=pet.dog_park&filter=rect:-87.80122308044409,42.01504297890354,-87.51437691955522,41.728586465138434&limit=20&apiKey=1d9fd57fb2b14fb5bfe2315af8475c59`).then((response) => { setArrayOfDogParks(response.data.features)})
+    axios.get(`https://api.geoapify.com/v2/places?categories=pet.dog_park&filter=rect:-87.80122308044409,42.01504297890354,-87.51437691955522,41.728586465138434&limit=20&apiKey=1d9fd57fb2b14fb5bfe2315af8475c59`).then((response) => { setArrayOfDogParks(response.data)})
   }
 
   const mapRef = useRef()
+  const geojson = arrayOfDogParks
 
-  const onMapLoad = useCallback(() => {
-    mapRef.current.on('click', (e) => new mapboxgl.Marker().setLngLat([e.lngLat.lng, e.lngLat.lat])).addTo(mapRef.current)
-  }, [])
 
+  const layerStyle = {
+    id: 'point',
+    type: 'circle',
+    paint: {
+      'circle-radius': 10,
+      'circle-color': '#007cbf'
+    }
+    
+  };
+  const whenClicked = () => {
+    console.log("clicked")
+  }
+  
+  console.log(arrayOfDogParks)
   return (
     <div>
       <Map ref={mapRef}  initialViewState={{
@@ -33,7 +45,13 @@ function AlternateMap(props) {
         height: '100vh'
       }} mapStyle='mapbox://styles/mapbox/streets-v11'
       mapboxAccessToken='pk.eyJ1IjoianByaWNlNDQiLCJhIjoiY2wybWZyZ3hmMDR1bTNrcGszYzV2OGl3MSJ9.ShuHeiSnowF4fYxU9MGVHQ'
-      onLoad={onMapLoad}></Map>
+      >
+        <Source id="dog-parks" type="geojson" data={geojson} onClick={whenClicked} >
+          <Layer {...layerStyle} />
+        </Source>
+        <NavigationControl />
+        <GeolocateControl />
+      </Map>
     </div>
   )
 }
