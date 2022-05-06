1421f3cf616675dbc88748aa7a9862ff59ae51d5
jennytoc <toctocanj@gmail.com>
2022-05-06 13:18:48 -0600

    Changed tinder dog

--- frontend/doggy-pile/src/pages/tinder/DoggyTinder.js
+++ frontend/doggy-pile/src/pages/tinder/DoggyTinder.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import TinderCard from 'react-tinder-card'
 import DoggyPileAPI from "../../api/DoggyPileAPI";
-import Corgi from "../../images/corgi.png"
+import Doggo from "../../images/tinder-dog.jpg"
 import "./TinderStyles.scss"
 
 function DoggyTinder() {
@@ -30,13 +30,11 @@ function DoggyTinder() {
 
   return (
     <div>
-      <link href='https://fonts.googleapis.com/css?family=Damion&display=swap' rel='stylesheet' />
-      <link href='https://fonts.googleapis.com/css?family=Alatsi&display=swap' rel='stylesheet' />
       <h1>React Tinder Card</h1>
       <div className='d-flex justify-content-center cardContainer'>
         {dogProfilesList && dogProfilesList.map((dog) =>
           <TinderCard className='swipe' key={dog && dog.name} onSwipe={(dir) => swiped(dir, dog && dog.name)} onCardLeftScreen={() => outOfFrame(dog && dog.name)}>
-            <div style={{ backgroundImage: `url(${dog && dog.profile_pic ? dog.profile_pic : Corgi })` }} className='card'>
+            <div style={{ backgroundImage: `url(${dog && dog.profile_pic ? dog.profile_pic : Doggo })` }} className='card'>
               <h3>{dog && dog.name}</h3>
             </div>
           </TinderCard>
