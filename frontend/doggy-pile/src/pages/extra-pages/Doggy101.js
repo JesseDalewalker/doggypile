9d2dcb6410a3f74120b28fab1e24fb9afd9f5ea4
dragoneye888 <jmdalewalker@gmail.com>
2022-05-10 11:56:04 -0500

    Doggy 101 links able to open new window and accordian fixed

--- frontend/doggy-pile/src/pages/extra-pages/Doggy101.js
+++ frontend/doggy-pile/src/pages/extra-pages/Doggy101.js
@@ -36,7 +36,6 @@ function Doggy101() {
                 <Accordion.Header>Do I need to take my dog to dog parks?</Accordion.Header>
                 <Accordion.Body>Dogs are pack animals, very social and require regular interaction. Dog parks are a great way to get your dog exercise as well as socialization, if you don't have a dog park near by it's best to try to socialize your dog with other dogs owned by family and friends. The more time your dog spends with other dogs the better.</Accordion.Body>
               </Accordion.Item>
-              </Accordion>
               <Accordion.Item eventKey='3'>
                 <Accordion.Header>How much exercise does my dog need?</Accordion.Header>
                 <Accordion.Body>The specific amount of exercise your dog needs is very dependent of the breed, but all dogs to some extent need daily exercise. Some of the less active dog breeds only require small walks during the day, while others can need several hours of intense exercise. It is very important that if your dog is a more active dog breed that you allow it the abililty to run around and explore outside.</Accordion.Body>
@@ -49,15 +48,16 @@ function Doggy101() {
                 <Accordion.Header>Do I need to brush my dog's teeth?</Accordion.Header>
                 <Accordion.Body>Dog's dental care is very important and often overlooked and is the reason that a majority of dogs show signs of canine periodontal disease by the age of three. While brushing your dogs teeth is an excellent way to prevent plaque buildup, most dogs aren't too fond of the process. Luckily there are many options to choose from with dog tooth wipes, dental treats and chews that do a great job of removing plaque, and even proffesional cleanings by veterinarian.</Accordion.Body>
               </Accordion.Item>
+            </Accordion>
           </Col>
           <Col>
             <h2 className='question-header'>Helpful Links!</h2>
             <div className='links-div'>
               <ListGroup variant="flush">
-                <ListGroup.Item action href="https://www.rspcapetinsurance.org.au/pet-care/dog-care/what-should-feed-dog#:~:text=It%20is%20entirely%20acceptable%20to,need%20to%20be%20aware%20of.">More information on what to feed your dog available here!</ListGroup.Item>
-                <ListGroup.Item action href="https://www.campbellrivervet.com/often-take-dog-vet/#:~:text=Annual%20wellness%20exam,any%20concerns%20with%20your%20vet.">More information on veterinarian visits available here!</ListGroup.Item>
-                <ListGroup.Item action href="https://www.akc.org/expert-advice/health/keep-dog-teeth-clean/">More information on doggy dental care available here!</ListGroup.Item>
-                <ListGroup.Item action href="https://www.akc.org/expert-advice/health/how-much-exercise-does-dog-need/">More information on exercising your dog available here!</ListGroup.Item>
+                <ListGroup.Item target="_blank" action href="https://www.rspcapetinsurance.org.au/pet-care/dog-care/what-should-feed-dog#:~:text=It%20is%20entirely%20acceptable%20to,need%20to%20be%20aware%20of.">More information on what to feed your dog available here!</ListGroup.Item>
+                <ListGroup.Item target="_blank" action href="https://www.campbellrivervet.com/often-take-dog-vet/#:~:text=Annual%20wellness%20exam,any%20concerns%20with%20your%20vet.">More information on veterinarian visits available here!</ListGroup.Item>
+                <ListGroup.Item target="_blank" action href="https://www.akc.org/expert-advice/health/keep-dog-teeth-clean/">More information on doggy dental care available here!</ListGroup.Item>
+                <ListGroup.Item target="_blank" action href="https://www.akc.org/expert-advice/health/how-much-exercise-does-dog-need/">More information on exercising your dog available here!</ListGroup.Item>
               </ListGroup>
             </div>
           </Col>
@@ -66,7 +66,7 @@ function Doggy101() {
       <div className='d-flex justify-content-center links-div vet'>
         <div>
         <h2 className='question-header'>Find a veterinarian near you</h2>
-        <Button href="http://www.usa-veterinarians.com/" className='edit-btn'>Search Now</Button>
+        <Button target="_blank" href="http://www.usa-veterinarians.com/" className='edit-btn'>Search Now</Button>
         </div>
       </div>
       <img src={require('../../images/dogTongueOut.png')} alt="" className='dog-tongue-out'/>
